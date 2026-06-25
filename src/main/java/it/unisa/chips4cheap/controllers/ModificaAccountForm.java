f7c647a67f34df324b323c2bcc1432e9b8ea8502
Xnnea <A.STRATI1@STUDENTI.UNISA.IT>
2026-06-25 18:23:17 +0200

    CODE: aggiunta ModificaAccountForm.java modififcaAccount.jsp, REFACTORING: areaPersonale.jsp e del suo controller AreaPErsonale

--- src/main/java/it/unisa/chips4cheap/controllers/ModificaAccountForm.java
+++ src/main/java/it/unisa/chips4cheap/controllers/ModificaAccountForm.java
@@ -8,16 +8,16 @@ import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 /**
- * Servlet implementation class AreaPersonale
+ * Servlet implementation class ModificaAccountForm
  */
-@WebServlet("/common/AreaPersonale")
-public class AreaPersonale extends HttpServlet {
+@WebServlet("/common/ModificaAccountForm")
+public class ModificaAccountForm extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public AreaPersonale() {
+    public ModificaAccountForm() {
         super();
         // TODO Auto-generated constructor stub
     }
@@ -27,7 +27,7 @@ public class AreaPersonale extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		request.getRequestDispatcher("/WEB-INF/views/common/areaPersonale.jsp").forward(request, response);
+		request.getRequestDispatcher("/WEB-INF/views/common/modificaAccount.jsp").forward(request, response);
 	}
 
 	/**
